844ee46976a8631720d6a3b9b5fb7ed62d639028
alex-orlov <67315235+iobox420@users.noreply.github.com>
2021-03-21 16:38:37 +0500

    work-init

--- src/App.js
+++ src/App.js
@@ -1,42 +1,41 @@
-import React from "react"
-import "./App.css"
-
-import Profile from "./components/Profile/Profile"
-import Dialogs from "./components/Dialogs/Dialogs"
-import Header from "./components/Header/Header"
-import Navbar from "./components/Navbar/Navbar"
-import {BrowserRouter, Route} from "react-router-dom"
-import {updateNewPostText} from "./redux/state"
+import React from "react";
+import "./App.css";
+/*init*/
+import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
+import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
+import { BrowserRouter, Route } from "react-router-dom";
+import { updateNewPostText } from "./redux/state";
 
 const App = (props) => {
-    return (
-        <BrowserRouter>
-            <div className="app-wrapper">
-                <Header/>
-                <Navbar/>
-                <div className="app-wrapper-content">
-                    <Route
-                        path="/dialogs"
-                        render={() => (
-                            <Dialogs messagesPages={props.appstate.messagesPages}/>
-                        )}
-                    />
-                    <Route
-                        path="/profile"
-                        render={() => (
-
-                            <Profile
-                                profilePage={props.appstate.profilePage}
-                                newPostText={props.appstate.profilePage.newPostText}
-                                addPost={props.addPost}
-                                updateNewPostText={props.updateNewPostText}
-                            />
-                        )}
-                    />
-                </div>
-            </div>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className="app-wrapper-content">
+          <Route
+            path="/dialogs"
+            render={() => (
+              <Dialogs messagesPages={props.appstate.messagesPages} />
+            )}
+          />
+          <Route
+            path="/profile"
+            render={() => (
+              <Profile
+                profilePage={props.appstate.profilePage}
+                newPostText={props.appstate.profilePage.newPostText}
+                addPost={props.addPost}
+                updateNewPostText={props.updateNewPostText}
+              />
+            )}
+          />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
